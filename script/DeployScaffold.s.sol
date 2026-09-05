48d0d5cad79dbde09d318e279be713ba3ed5ddc5
Akshad gujarkar <akshadgujarkar12@gmail.com>
2026-09-05 14:34:08 +0530

    feat(script): deploy EntryPoint and fund Paymaster in DeployScaffold

--- script/DeployScaffold.s.sol
+++ script/DeployScaffold.s.sol
@@ -3,19 +3,41 @@ pragma solidity ^0.8.28;
 
 import {Script, console} from "forge-std/Script.sol";
 import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
+import {EntryPoint} from "account-abstraction/core/EntryPoint.sol";
 import {SmartAccountFactory} from "../src/SmartAccountFactory.sol";
 import {Paymaster} from "../src/Paymaster.sol";
 
 contract DeployScaffold is Script {
-    address constant ENTRY_POINT = 0x0000000071727De22E5E9d8BAf0edAc6f37da032;
+    address constant CANONICAL_ENTRY_POINT = 0x0000000071727De22E5E9d8BAf0edAc6f37da032;
 
     function run() external {
         vm.startBroadcast();
-        SmartAccountFactory factory = new SmartAccountFactory(IEntryPoint(ENTRY_POINT));
-        Paymaster paymaster = new Paymaster(IEntryPoint(ENTRY_POINT), msg.sender);
+
+        // 1. Deploy EntryPoint (or reuse if canonical already has bytecode)
+        address entryPointAddr;
+        if (CANONICAL_ENTRY_POINT.code.length > 0) {
+            entryPointAddr = CANONICAL_ENTRY_POINT;
+        } else {
+            EntryPoint ep = new EntryPoint();
+            entryPointAddr = address(ep);
+        }
+
+        // 2. Deploy SmartAccountFactory pointing to EntryPoint
+        SmartAccountFactory factory = new SmartAccountFactory(IEntryPoint(entryPointAddr));
+
+        // 3. Deploy Paymaster
+        Paymaster paymaster = new Paymaster(IEntryPoint(entryPointAddr), msg.sender);
+
+        // 4. Fund Paymaster with 2 ETH deposit in EntryPoint for gas sponsorship
+        IEntryPoint(entryPointAddr).depositTo{value: 2 ether}(address(paymaster));
+
         vm.stopBroadcast();
 
-        console.log("SmartAccountFactory deployed to:", address(factory));
-        console.log("Paymaster deployed to:", address(paymaster));
+        console.log("==================================================");
+        console.log("EntryPoint deployed to:          ", entryPointAddr);
+        console.log("SmartAccountFactory deployed to: ", address(factory));
+        console.log("Paymaster deployed to:           ", address(paymaster));
+        console.log("Paymaster funded with 2 ETH deposit in EntryPoint!");
+        console.log("==================================================");
     }
 }
